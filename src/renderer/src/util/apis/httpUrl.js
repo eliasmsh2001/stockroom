9d4c022a7d0c6540afbeb2f32700fe46cae7613a
eliasmsh2001 <eislymsh2001@gmail.com>
2026-02-01 21:00:23 +0200

    new commit

--- src/renderer/src/util/apis/httpUrl.js
+++ src/renderer/src/util/apis/httpUrl.js
@@ -2,6 +2,6 @@ import { QueryClient } from '@tanstack/react-query'
 
 export const queryClient = new QueryClient()
 
-const url = 'http://127.0.0.1:3001'
-// const url = 'http://192.168.30.34:3001'
+// const url = 'http://127.0.0.1:3001'
+const url = 'http://192.168.30.34:3001'
 export default url
